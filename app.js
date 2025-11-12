d7eec5182c6bc7bf6e0fe8be600b4f746e2fed76
Lochana108 <deshanlochana2@gmail.com>
2025-11-12 11:37:17 +0530

    table alignment

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 let tblCountries = document.getElementById("tblCountries");
 
-let tableBody = `<tr>
-            <th class="text-center">Name</th>
-            <th class="text-center">Flag</th>
-        </tr>`;
+let tableBody = `<thead><tr>
+            <th>Name</th>
+            <th>Flag</th>
+        </tr></thead>`;
 
 fetch("https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags")
 .then((res)=>res.json())
 .then(data=>{
     data.forEach(element => {
         tableBody+=`<tr>
-                        <td><h3>${element.name.common}</h3><br>${element.name.official}</td>
-                        <td class="text-center"><img src = "${element.flags.svg}" width = "250"></td>
+                        <td><h1>${element.name.common}</h1><br>${element.name.official}</td>
+                        <td><img src="${element.flags.png}""></td>
                     </tr>`
         console.log(element.name.common);
     });
