6ba4603ccf3847f0e019ecd8c4ac16e9b2b57af5
Lochana108 <deshanlochana2@gmail.com>
2025-11-12 14:19:03 +0530

    map added

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ let tableBody = `<thead><tr>
             <th>Flag</th>
         </tr></thead>`;
 
-fetch("https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags")
+fetch("https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags,maps")
 .then((res)=>res.json())
 .then(data=>{
     data.forEach(element => {
         tableBody+=`<tr>
-                        <td><h1>${element.name.common}</h1><br>${element.name.official}</td>
+                        <td><h1>${element.name.common}</h1><br>
+                        Official Name : ${element.name.official}
+                        <p>Capital : ${element.capital}
+                        <p>Region : ${element.region}
+                        <p>Population : ${element.population}<br>
+                        <a href="${element.maps.googleMaps}" class="btn btn-primary">Go to map</a>
+                        </td>
                         <td><img src="${element.flags.png}""></td>
                     </tr>`
         console.log(element.name.common);
