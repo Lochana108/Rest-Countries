b03b4821e3d30b1a42900b721430bb3556190d55
Lochana108 <deshanlochana2@gmail.com>
2025-11-12 08:47:01 +0530

    table updated

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let tblCountries = document.getElementById("tblCountries");
 
 let tableBody = `<tr>
-            <th>Name</th>
-            <th>Flag</th>
+            <th class="text-center">Name</th>
+            <th class="text-center">Flag</th>
         </tr>`;
 
 fetch("https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags")
@@ -10,8 +10,8 @@ fetch("https://restcountries.com/v3.1/all?fields=name,capital,region,population,
 .then(data=>{
     data.forEach(element => {
         tableBody+=`<tr>
-                        <td>${element.name.common}</td>
-                        <td><img src = "${element.flags.svg}" width = "50"></td>
+                        <td><h3>${element.name.common}</h3><br>${element.name.official}</td>
+                        <td class="text-center"><img src = "${element.flags.svg}" width = "250"></td>
                     </tr>`
         console.log(element.name.common);
     });
